5c824cafad718e36c3758e334953f79b8234435a
Patrick Wu <wotingwu@live.com>
2018-11-26 09:20:39 +0800

    new openCV system

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,51 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+using namespace std;
+using namespace cv;
+
+extern "C"
+void JNICALL Java_fyp2018_hkust_vorusingopencv_MainActivity_salt(JNIEnv *env, jobject instance,
+                                                          jlong matAddrGray) {
+    Mat frame = *(Mat *) matAddrGray;
+
+    String face_cascade_name = "/storage/emulated/0/vor/haarcascade_frontalface_alt.xml";
+    String eyes_cascade_name = "/storage/emulated/0/vor/haarcascade_eye_tree_eyeglasses.xml";
+    CascadeClassifier face_cascade;
+    CascadeClassifier eyes_cascade;
+
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return; };
+
+    std::vector<Rect> faces;
+    Mat frame_gray;
+
+    cvtColor( frame, frame_gray, CV_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    //Faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+
+    for( size_t i = 0; i < faces.size(); i++ )
+    {
+        rectangle(frame, Point(faces[i].x + faces[i].width, faces[i].y), Point(faces[i].x, faces[i].y + faces[i].height),Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+
+        //Eye
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+
+        for( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+}
