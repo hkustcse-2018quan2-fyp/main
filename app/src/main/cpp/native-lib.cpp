9951654ee13e45d4acba7296744e0599e9e2d830
dhaval1704 <32483380+dhaval1704@users.noreply.github.com>
2019-01-10 13:59:51 +0800

    Added face recognizer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,10 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/face.hpp>
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 using namespace cv;
 using namespace cv::face;;
@@ -34,6 +38,105 @@ void JNICALL Java_fyp2018_hkust_vorusingopencv_MainActivity_salt(JNIEnv *env, jo
                   Point(faces[i].x, faces[i].y + faces[i].height), Scalar(255, 0, 0), 2, 8, 0);
 
         Mat faceROI = frame_gray(faces[i]);
+    }
+
+    static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+        std::ifstream file(filename.c_str(), ifstream::in);
+        if (!file) {
+            string error_message = "No valid input file was given, please check the given filename.";
+            CV_Error(CV_StsBadArg, error_message);
+        }
+        string line, path, classlabel;
+        while (getline(file, line)) {
+            stringstream liness(line);
+            getline(liness, path, separator);
+            getline(liness, classlabel);
+            if(!path.empty() && !classlabel.empty()) {
+                images.push_back(imread(path, 0));
+                labels.push_back(atoi(classlabel.c_str()));
+            }
+        }
+    }
+
+    int main(int argc, const char *argv[]) {
+
+        // Get the path to your CSV:
+        string fn_haar = string(argv[1]);
+        string fn_csv = string(argv[2]);
+        int deviceId = atoi(argv[3]);
+        // These vectors hold the images and corresponding labels:
+        vector<Mat> images;
+        vector<int> labels;
+
+        // Read in the data (fails if no valid input filename is given)
+        try {
+            read_csv(fn_csv, images, labels);
+        } catch (cv::Exception& e) {
+            cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+            exit(1);
+        }
+        // Get the height from the first image.
+        int im_width = images[0].cols;
+        int im_height = images[0].rows;
+
+        // Create a FaceRecognizer and train it on the given images:
+        Ptr<FaceRecognizer> model = FisherFaceRecognizer::create();
+        model->train(images, labels);
+
+        CascadeClassifier haar_cascade;
+        haar_cascade.load(fn_haar);
+        // Get a handle to the Video device:
+        VideoCapture cap(deviceId);
+        // Check if we can use this device:
+        if(!cap.isOpened()) {
+            cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
+            return -1;
+        }
+        // Holds the current frame from the Video device:
+        Mat frame;
+        for(;;) {
+            cap >> frame;
+            // Clone the current frame:
+            Mat original = frame.clone();
+            // Convert the current frame to grayscale:
+            Mat gray;
+            cvtColor(original, gray, CV_BGR2GRAY);
+            // Find the faces in the frame:
+            vector< Rect_<int> > recognised_faces;
+            haar_cascade.detectMultiScale(gray, recognised_faces);
+            // At this point you have the position of the faces in
+            // faces. Now we'll get the faces, make a prediction and
+            // annotate it in the video. Cool or what?
+            for(int i = 0; i < recognised_faces.size(); i++) {
+                // Process face by face:
+                Rect face_i = recognised_faces[i];
+                // Crop the face from the image.
+                Mat face = gray(face_i);
+
+                Mat face_resized;
+                cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
+                // Perform prediction
+                int prediction = model->predict(face_resized);
+         
+                rectangle(original, face_i, CV_RGB(0, 255,0), 1);
+                // Create the text we will annotate the box with:
+                string box_text = format("Prediction = %d", prediction);
+                // Calculate the position for annotated text (make sure we don't
+                // put illegal values in there):
+                int pos_x = std::max(face_i.tl().x - 10, 0);
+                int pos_y = std::max(face_i.tl().y - 10, 0);
+                // And now put it into the image:
+                putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 2.0);
+            }
+            // Show the result:
+            imshow("face_recognizer", original);
+            // And display it:
+            char key = (char) waitKey(20);
+            // Exit this loop on escape:
+            if(key == 27)
+                break;
+        }
+        return 0;
     }
 }
 }
